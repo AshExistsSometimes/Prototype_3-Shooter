21b71209ca6526fa311317d01361063ecf4c367d
Ashley N <ewannicholson2004@gmail.com>
2024-11-27 22:09:13 +0000

    Fixed revolver
    dropped Revolver damage so it doesn't one shot anymore

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.PlayerSettings;
 
 public class Stats : MonoBehaviour
 {
